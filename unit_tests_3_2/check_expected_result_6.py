88855f4c206c2749dd5550e83f37607d23025406
glartir <sidorenko.slava@gmail.com>
2020-06-24 11:45:40 +0300

    3_2 finished

---
+++ unit_tests_3_2/check_expected_result_6.py
@@ -0,0 +1,5 @@
+
+print("Let's count together: {}, then goes {}, and then {}".format("one", "two", "three"))
+actual_result = "abrakadabra"
+print(f"Wrong text, got {actual_result}, something wrong")
+assert abs(-42) == -42 , "Should be absolute value of a number"
