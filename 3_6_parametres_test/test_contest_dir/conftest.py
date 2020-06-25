19e1f9649d3b0151cba2006923cdb2fc3b230b7b
glartir <sidorenko.slava@gmail.com>
2020-06-25 10:59:36 +0300

    good conftest with languge parameter

--- 3_6_parametres_test/test_contest_dir/conftest.py
+++ 3_6_parametres_test/test_contest_dir/conftest.py
@@ -2,22 +2,36 @@
 
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
 
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default="chrome",
+                     help="Choose browser: chrome or firefox")
+    parser.addoption('--languge', action='store', default="en",
                      help="Choose browser: chrome or firefox")
 
 
 @pytest.fixture(scope="function")
 def browser(request):
     browser_name = request.config.getoption("browser_name")
-    browser = None
+    user_language=request.config.getoption("language")
+    #browser = None
     if browser_name == "chrome":
+
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+
         print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome()
+        browser = webdriver.Chrome(options=options)
+        # browser = webdriver.Chrome()
     elif browser_name == "firefox":
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", user_language)
         print("\nstart firefox browser for test..")
-        browser = webdriver.Firefox()
+        #browser = webdriver.Firefox()
+        browser = webdriver.Firefox(firefox_profile=fp)
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
